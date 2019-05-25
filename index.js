096a67e0a115c7738a3dc7688c87bf619f745b16
Aujoulator <axel@medgo.fr>
2019-05-25 11:08:01 +0200

    ENV file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
+
+require("dotenv").config();
+
 const BootBot = require('./lib/BootBot');
 
 const bot = new BootBot({
-    accessToken: 'EAAIaxxzUj9MBAEq7gDSJSw4WkO2h4p5a3SLOaHxEW3ixxWWq5VtujSmPT04JZCxZAtnVEKHbljOCverfLJKusZCI7WczOZB2sc5rnLIIXL68CkHus9spA5QaNCq2DH4B8wca591JZASi5xPSLZB2vSxTMFSZBDDgpnqHP3ikrkmbQZDZD',
-    verifyToken: 'speach_for_medidays',
-    appSecret: 'FB_APP_SECRET'
+    accessToken: process.env.FB_ACCESS_TOKEN,
+    verifyToken: process.env.FB_VERIFY_TOKEN,
+    appSecret: process.env.FB_APP_SECRET
 });
 
-module.exports = BootBot;
+bot.start();
