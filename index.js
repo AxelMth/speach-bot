f1f90224ffed3374f2d099b1055caa4720c429e0
pjollymed <pierrick@medgo.fr>
2019-05-26 12:16:59 +0200

    merge reminder

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ Bot.hear("Testloc", (payload, chat) => {
 Bot.on('attachment', (payload, chat) => {
   console.log("plural");
   console.log(payload.message.attachments);
-  console.log(payload.message.attachments["payload"]);
+  console.log(payload.message.attachments[3]);
   console.log("$$$$coordinates")
   console.log(payload.message.attachments.payload.coordinates);
 
