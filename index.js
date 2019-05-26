da2e88b008e26be79ed8128edbbf0d48263a3c9f
Aujoulator <axel@medgo.fr>
2019-05-26 12:01:52 +0200

    WI

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 'use strict';
 
 require("dotenv").config();
@@ -11,3 +12,45 @@ const bot = new BootBot({
 });
 
 bot.start();
+=======
+"use strict";
+require("dotenv").config();
+const Reminder = require("./models/Reminder").default;
+const Bot = require("./bot").bot;
+const Scenario = require("./lib/Scenario");
+
+const getFromGoogleApi = require("./lib/utils/googleApi").getFromGoogleApi;
+
+const scenario1 = new Scenario(Bot, [
+  {
+    listener: /Matin|Aprèm|Nuit/i,
+    actions: [
+      {
+        type: "say object",
+        text: "À quelle heure ?",
+        quickReplies: ["jhh", "hjcdduifbd"]
+      }
+    ]
+  },
+  {
+    type: "quickReplies",
+    listener: "quick_reply:ORDO_NO_MORE",
+    text: "Ok! Je peux te chercher un gynécologue ‍! J’ai besoin de ton adresse stp",
+    actions: [
+      {
+        "content_type":"text",
+        "title": "C’est mort :crâne:",
+        "payload": "<HOUR_SET_21H>",
+      },
+      {
+        "content_type": "location",
+        "title": "Géolocalise-moi !"
+      }
+    ],
+  }
+]);
+
+scenario1.playScenario();
+
+Bot.start(3000);
+>>>>>>> Stashed changes
